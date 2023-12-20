872c1968c1015f4aa5ff07517f66a3e3026fa923
Samuel Martins <samuel.martins4.sm@gmail.com>
2023-12-20 23:58:16 +0300

    chore: implement redis tasks

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,20 +2,49 @@
 """
 create a web cach
 """
-import redis
+
 import requests
-rc = redis.Redis()
-count = 0
+import time
+from functools import wraps
+
+# Dictionary to store cached results and their access counts
+cache = {}
 
 
-def get_page(url: str) -> str:
+def cache_page_result(func):
+    @wraps(func)
+    def wrapper(url):
+        # Check if the result is in the cache and not expired
+        if url in cache and time.time() - cache[url]["timestamp"] < 10:
+            # Increment the access count
+            cache[url]["count"] += 1
+            print(f"Cache hit! Count: {cache[url]['count']}")
+            return cache[url]["content"]
+
+        # Cache miss or expired, fetch the page
+        content = func(url)
+
+        # Update the cache
+        cache[url] = {"content": content, "count": 1, "timestamp": time.time()}
+
+        return content
+
+    return wrapper
+
+
+@cache_page_result
+def get_page(url):
     """ get a page and cach value"""
-    rc.set(f"cached:{url}", count)
-    resp = requests.get(url)
-    rc.incr(f"count:{url}")
-    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
-    return resp.text
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for bad responses
+        return response.text
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching page: {e}")
+        return None
 
 
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+# Example usage
+# url = "http://slowly.robertmurray.co.uk"
+# page_content = get_page(url)
+# print(page_content)
