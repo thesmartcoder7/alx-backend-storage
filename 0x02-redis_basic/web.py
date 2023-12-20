5f2c7ecf35132650e33d74c3338e47e382d45bca
Samuel Martins <samuel.martins4.sm@gmail.com>
2023-12-20 23:48:13 +0300

    chore: implement redis tasks

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,21 +1,56 @@
 #!/usr/bin/env python3
-"""
-create a web cach
-"""
-import redis
+""" implement get_page function """
 import requests
-rc = redis.Redis()
-count = 0
+import redis
+import functools
+from typing import Callable
 
 
-def get_page(url: str) -> str:
-    """ get a page and cach value"""
-    rc.set(f"cached:{url}", count)
-    resp = requests.get(url)
-    rc.incr(f"count:{url}")
-    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
-    return resp.text
+def count_calls(method: Callable) -> Callable:
+    """
+    this is a Decorator to count the number of times a method is called.
+
+    Args:
+        method (Callable): The method to be decorated.
+
+    Returns:
+        Callable: The decorated method.
+    """
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = f"count:{args[0]}"
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+class Web:
+    """
+    this is a Web class for getting HTML content of a URL and caching it.
+
+    Attributes:
+        _redis (Redis): Instance of the Redis client.
+    """
+
+    def __init__(self):
+        """
+        Web class constructor.
+        """
+        self._redis = redis.Redis()
+
+    @count_calls
+    def get_page(self, url: str) -> str:
+        """
+        Get the HTML content of a URL and cache it.
+
+        Args:
+            url (str): The URL to get the HTML content from.
+
+        Returns:
+            str: The HTML content of the URL.
+        """
+        response = self._redis.get(url)
+        if response is None:
+            response = requests.get(url).text
+            self._redis.setex(url, 10, response)
+        return response
